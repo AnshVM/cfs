44c95635bd96ac1bf49d0bdd3a4fdf3a8d4d4b4f
AnshVM <anshmalik2002@gmail.com>
2022-12-13 23:33:51 +0530

    removed console.logs

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import shell from 'shelljs';
 const inputJSON = (): Input => {
     const data = fs.readFileSync(process.argv[2]).toString();
     shell.cd(process.argv[3]);
-    console.log(`cd into ${process.argv[3]}`);
     return JSON.parse(data) as Input;
 }
 
@@ -14,18 +13,14 @@ interface Input {
 }
 
 const genf = (input: Input) => {
-    console.log(input);
     for (const key of Object.keys(input)) {
-        console.log(key);
         if (Array.isArray(input[key])) {
 
             (input[key] as string[]).forEach(f => {
                 shell.exec(`touch ${f}`)
-                console.log(`Creating file ${f}`);
             })
         }
         else {
-            console.log(`Creating dir ${key}`);
             shell.exec(`mkdir ${key}`);
             shell.cd(key);
             genf(input[key] as Input);
